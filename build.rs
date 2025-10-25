534e41d95320968c7ac7e4bc2442e4740e865c22
lkarch <lkarch@protonmail.com>
2025-10-25 16:08:16 -0400

    Fixed

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
 fn main() {
-    println!("cargo::rerun-if-changed=.git/HEAD");
+    println!("cargo::rerun-if-changed=.git/refs/heads/main");
     let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap_or_default();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
